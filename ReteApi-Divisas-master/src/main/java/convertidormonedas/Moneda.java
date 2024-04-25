79d316385124e165d5bc01909ffecb9e6ccc0d5a
R-Mutt22 <118209167+R-Mutt22@users.noreply.github.com>
2024-04-25 15:15:45 -0300

    Add files via upload

---
+++ ReteApi-Divisas-master/src/main/java/convertidormonedas/Moneda.java
@@ -0,0 +1,17 @@
+package convertidormonedas;
+
+public class Moneda {
+    private String codigoBase;
+    public String getCodigoBase() {
+        return codigoBase;
+    }
+
+    public void setCodigoBase(String codigoBase) {
+        this.codigoBase = codigoBase;
+    }
+    public Moneda() {
+    }
+    public Moneda(String codigoBase) {
+        this.codigoBase = codigoBase;
+    }
+}
